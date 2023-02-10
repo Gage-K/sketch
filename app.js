d6bc25cbc2c7b3594388974187d2e77207b01bc6
Gage-K <krausegage@gmail.com>
2023-02-09 23:14:44 -0500

    Add "row" and "col" classes to divs in getGrid()

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ function getGrid (){
 
     for (i = 0; i < 16; i++){ // iterates through rows
         let row = document.createElement("div");
+        row.classList.add("row");
         pad.appendChild(row);
         for (j = 0; j < 16; j++){ // iterates through columns
             let col = document.createElement("div");
+            col.classList.add("col");
             row.appendChild(col);
             col.textContent = j; // dummy text for checking div creation
             //gridArray.push(gridFiller);
