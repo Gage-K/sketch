e29fc8b9190c557264a88a8b2fbabbe7595cc167
Gage-K <krausegage@gmail.com>
2023-02-09 23:09:58 -0500

    Add DOM manipulation to getGrid() Creates a 16 row divs with 16 column divs per row

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ function getGrid (){
     let gridArray = [];
     let gridFiller = 1;
 
-    for (i = 0; i < 8; i++){ // iterates through rows
-        for (j = 0; j < 8; j++){ // iterates through columns
-            gridArray.push(gridFiller);
-            gridFiller++;
+    for (i = 0; i < 16; i++){ // iterates through rows
+        let row = document.createElement("div");
+        pad.appendChild(row);
+        for (j = 0; j < 16; j++){ // iterates through columns
+            let col = document.createElement("div");
+            row.appendChild(col);
+            col.textContent = j; // dummy text for checking div creation
+            //gridArray.push(gridFiller);
+            //gridFiller++;
         };
     };
 
-    console.log(gridArray);
+    //console.log(gridArray);
 };
 
 getGrid();
