d3788b9b136ba1993c9567a450844fb1f38923c0
Gage-K <krausegage@gmail.com>
2023-02-11 23:36:02 -0500

    Add CSS grid manipulation to getGrid()

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ function getGrid (){
     let gridArray = [];
     let gridFiller = 1;
 
-    for (i = 0; i < 16; i++){ // iterates through col
-        let col = document.createElement("div");
-        col.classList.add("col");
-        pad.appendChild(col);
-        for (j = 0; j < 16; j++){ // iterates through row
-            let row = document.createElement("div");
-            row.classList.add("row");
-            col.appendChild(row);
-            row.textContent = j; // dummy text for checking div creation
-            //gridArray.push(gridFiller);
-            //gridFiller++;
-        };
+    let rowAmt = 16;
+    let cellAmt = rowAmt * rowAmt;
+
+    pad.style.display = 'grid';
+    pad.style.gridTemplateRows = `repeat(${rowAmt} 1fr)`;
+    pad.style.gridTemplateColumns = `repeat(${rowAmt} 1fr)`;
+
+    let row = 1;
+    let col = 1;
+
+    for (i = 0; i < cellAmt; i++){
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.gridRow = row;
+        cell.style.gridColumn = col;
+        cell.textContent = i;
+
+        col += 1;
+        if (col === rowAmt + 1){
+            row += 1;
+            col = 1;
+        }
+        
+
+        pad.appendChild(cell);
     };
 
     //console.log(gridArray);
